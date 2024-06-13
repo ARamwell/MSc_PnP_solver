9a7599db039cf18499fc3f8e87ddeb359cc663d5
alyss <alyss@DESKTOP-7CCIG3J>
2024-06-13 11:19:58 +0200

    Functional for calculating P3P for a number of images in a file. Beginning to add graphing functionality.

--- code/P3P_processVideo.m
+++ code/P3P_processVideo.m
@@ -2,29 +2,52 @@
 
 %Perform P3P on a prerecorded video of a checkerboard
 
+%Initialise variables
+t_hist_cust = zeros(3,1); %trajectory history, in the form of translation vector
+t_hist_builtin = zeros(3,1);
+
+%Initialise trajectory plot
+%figure;
+trajPlotFig = figure(); 
+trajPlotAx = axes('Parent', trajPlotFig);%'XLim', [-5000, 5000], 'YLim', [-5000, 5000], 'ZLim', [0, 5000]);%, 'XLabel', 'x (mm)', 'YLabel', 'y (mm)','ZLabel', 'z (mm)');
+
+%trajPlot = plot3(t_hist_cust(1,:), t_hist_cust(2,:), t_hist_cust(3,:));
+
+%plot3(trajPlotAx, [],[],[]);
+xlim(trajPlotAx, [-5000, 5000]);
+ylim(trajPlotAx,[-5000, 5000]);
+zlim(trajPlotAx,[0, 5000]);
+view(trajPlotAx, 3);
+%hold on
+%xlabel('x (mm)');
+%ylabel('y (mm)');
+%zlabel('z (mm)');
+hold on
+trajPlot = plot3(trajPlotAx,[1000],[1000],[1000], '-or');
+%linkdata(trajPlotFig, 'on');
+x =[1000 2000];
+y = [1000 2000];
+z=[1000 1000];
+set(trajPlot, 'XData', x, 'YData', y, 'ZData', z);
+drawnow
 %Locate video
-[vidFile,vidLocation] = uigetfile('','Select a video to process');
-dirOutput = dir(fullfile(vidLocation,vidFile));
-fileNames = {dirOutput.name}'
-numFrames = numel(fileNames)
+files_ds = fileDatastore(fullfile(uigetdir), 'ReadFcn', @importdata, "FileExtensions",".jpg");
+allFileNames = files_ds.Files;
+numFiles = length(allFileNames);
 
-I = imread(fullfile(vidLocation, vidFile));
-vidImageSeq_BW = zeros([size(I) numFrames],class(I));
-vidImageSeq_BW(:,:,1) = rgb2gray(I);
+%For first iteration
+I_colour = imread(string(allFileNames(1)));
+I_gray = rgb2gray(I_colour);
+I_allFrames = I_gray;
 
-for p = 2:numFrames
-    I = imread(fullfile(vidLocation, vidFile));
-    vidImageSeq_BW(:,:,p) = rgb2gray(I);
+
+%Loop to read all image files and put them into a multidim array
+for n=2: numFiles
+
+    I_colour = imread(string(allFileNames(n)));
+    I_gray = rgb2gray(I_colour);
+    I_allFrames = cat(3, I_allFrames, I_gray);
 end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%Maybe do some kind of processing here?
-%Convert to grayscale
-for p = 2:numFrames
-    vidImageSeq_BW(:,:,p) = rgb2gray(vidImageSeq(:,:,p))
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 
 
@@ -35,48 +58,49 @@ end
 mdl = "P3P_Grunert";
 open_system(mdl);
 simIn = Simulink.SimulationInput(mdl);
-%p3pMask = Simulink.Mask.get("P3P_Grunert/Perform P3P and RANSAC on checkerboard");
 
 %Set parameters
 checkerSize = [7, 10]; %number of squares
 checkerSquareLength = 23; %mm
 K = [1274.58238813250 0 627.693937913345; 0 1273.36371905619 405.441250468717; 0 0 1];
 
-%Pass to simulnk
-%p3pMask.Parameters(2).set('Value', checkerSize);
-%set_param(maskPath, 'checkerSize', checkerSize);
-%set_param(p3pMask, 'K', K);
-%set_param
-%set_param(p3pMask, 'checkerSquareEdgeLength', checkerSquareLength);
-
-%Create struct to pass parameters to simulation
-%simData = createInputDataset(mdl);
-%simData{2} = checkerSize;
-%simData{3} = K;
-%simData{4} = checkerSquareLength;
-%simIn =simIn.setBlockParameter("P3P_Grunert/Perform P3P and RANSAC on checkerboard", 'checkerSize', checkerSize);
-%simIn =simIn.setBlockParameter("P3P_Grunert/Perform P3P and RANSAC on checkerboard", 'intrinsicMat', K);
-%simIn =simIn.setBlockParameter("P3P_Grunert/Perform P3P and RANSAC on checkerboard", 'checkerEdgeLength', checkerSquareLength);
-
-
-simIn = simIn.setVariable('checkerSize', checkerSize, 'Workspace', mdl);
-simIn = simIn.setVariable('checkerSquareEdgeLength', checkerSquareLength, 'Workspace', mdl);
-simIn = simIn.setVariable('K', K, 'Workspace', mdl);
+set_param(mdl,"FastRestart","on")
 
 
 
+
+numFrames = numFiles;
 for q=1:numFrames
+    
+    %Extract current frame
+    I_current = I_allFrames(:,:,q);
 
-    %simData{1} = vidImageSeq_BW(:,:,q);
-    I_current = vidImageSeq_BW(:,:,q);
-    simIn = simIn.setVariable('intensityMatrix', I_current, 'Workspace', mdl);
-    %Load data into sim
-    %simIn = setExternalInput(simIn,inDS);
-    %set_param(p3pMask, 'intensityMatrix', vidImageSeq_BW(:,:,q));
-
+    %Run simulation
     out = sim(simIn);
+    
+    %Load new augmented Rt matrix results
+    Rt_new_cust = out.simout(:,:,1);
+    Rt_new_builtin = out.simout(:,:,2);
+
+    %Add new results to trajectory history
+    if q == 1
+        t_hist_cust = Rt_new_cust(1:3,4);
+        t_hist_builtin = Rt_new_cust(1:3,4);
+    else
+        t_hist_cust = cat(2, t_hist_cust, Rt_new_cust(1:3,4));
+        t_hist_builtin = cat(2, t_hist_builtin, Rt_new_builtin(1:3,4));
+    end
+
+
+    %Plot result
+    drawnow
+
+
+
 end
 
+figure
+trajScatter = scatter3(t_hist_cust(1,:), t_hist_cust(2,:), t_hist_cust(3,:))
 
 
 
--- code/builtinP3P.m
+++ code/builtinP3P.m
@@ -1,18 +1,22 @@
-function A = builtinP3P(imagePts_vert, worldPts_vert, focalLngth, principalPnt, imageSize, estTranslationVec, estRotationMat)
+function A = builtinP3P(imagePts_vert, worldPts_vert, focalLngth, principalPnt, imageSize, estTranslationVec, estRotationMat, plotCamera)
     
     intr = cameraIntrinsics(focalLngth, principalPnt, imageSize)
 
     worldPose = estworldpose(imagePts_vert, worldPts_vert, intr)
     A = worldPose.A;
 
-    pcshow(worldPts_vert,VerticalAxis="Y",VerticalAxisDir="down", ...
-    MarkerSize=30);
-    hold on
-    plotCamera(Size=10,Orientation=worldPose.R', ...
-        Location=worldPose.Translation);
-    hold on
-    plotCamera(Size=10,Orientation=estRotationMat', ...
-        Location=estTranslationVec,Color=[0 1 0]);
-    hold off
+    if plotCamera ==1
+        figure;
+        pcshow(worldPts_vert,VerticalAxis="Y",VerticalAxisDir="down", ...
+        MarkerSize=30);
+        
+        hold on
+        plotCamera(Size=10,Orientation=worldPose.R', ...
+            Location=worldPose.Translation);
+        hold on
+        plotCamera(Size=10,Orientation=estRotationMat', ...
+            Location=estTranslationVec,Color=[0 1 0]);
+        hold off
+    end 
 
 end
